f7195f02732aef4f6d4811f5c14f91e6069a218d
Ken18Ivan <clentivan.pacilan@g.msuiit.edu.ph>
2025-12-16 03:45:56 +0800

    vite config.js

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  build: {
+    // Increase the limit to 1000 KB (1 MB) to stop the warning
+    // NOTE: This doesn't fix performance, just hides the warning.
+    chunkSizeWarningLimit: 1000, 
+  }
+
 })
